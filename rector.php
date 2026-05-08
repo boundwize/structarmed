e194cbd93749589e44b8a2a3aaab79cc7d103f5f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-05-08 20:56:18 +0700

    fix

--- rector.php
+++ rector.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodParameterRector;
 use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
 
 return RectorConfig::configure()
