89bff6e2f20062c663a57936bd32e98747710ac8
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-05-13 08:19:56 +0700

    cs fix

--- src/Analyser/ClassNode.php
+++ src/Analyser/ClassNode.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Boundwize\StructArmed\Analyser;
 
 use function array_filter;
-use function array_values;
 use function end;
 use function explode;
 use function in_array;
