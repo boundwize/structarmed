aa333025b4c6ddeee0aba854da7c7221b6506739
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-06-01 12:01:17 +0700

    Disambiguate Source layer name when presets are combined with different source paths

---
+++ src/Preset/Presets/ResolvesSourceLayerName.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boundwize\StructArmed\Preset\Presets;
+
+use Boundwize\StructArmed\Architecture;
+
+use function implode;
+
+trait ResolvesSourceLayerName
+{
+    public const SOURCE_LAYER = 'Source';
+
+    public function resolveLayerName(Architecture $architecture): string
+    {
+        $sourcePaths    = $this->sourcePaths ?? [];
+        $existingLayers = $architecture->getLayers();
+
+        if (! isset($existingLayers[self::SOURCE_LAYER])) {
+            return self::SOURCE_LAYER;
+        }
+
+        if ((array) $existingLayers[self::SOURCE_LAYER] === $sourcePaths) {
+            return self::SOURCE_LAYER;
+        }
+
+        return self::SOURCE_LAYER . '[' . implode(',', $sourcePaths) . ']';
+    }
+}
