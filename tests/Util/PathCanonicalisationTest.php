7f3695cdcd4b7c3007dfdc14af15e1482a17e2b6
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-06-21 17:23:51 +0700

    fix canonicalize path

---
+++ tests/Util/PathCanonicalisationTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boundwize\StructArmed\Tests\Util;
+
+use Boundwize\StructArmed\Util\Path;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+use function bin2hex;
+use function file_exists;
+use function file_put_contents;
+use function mkdir;
+use function random_bytes;
+use function rmdir;
+use function sys_get_temp_dir;
+use function unlink;
+
+#[CoversClass(Path::class)]
+final class PathCanonicalisationTest extends TestCase
+{
+    public function testCanonicalisationMissIsNotCached(): void
+    {
+        $directory = sys_get_temp_dir() . '/structarmed-path-' . bin2hex(random_bytes(6));
+        $path      = $directory . '/./created.php';
+
+        mkdir($directory);
+
+        try {
+            $beforeCreation = Path::normalise($path, canonicalise: true);
+
+            file_put_contents($path, '<?php');
+
+            $afterCreation = Path::normalise($path, canonicalise: true);
+
+            $this->assertSame($path, $beforeCreation);
+            $this->assertNotSame($beforeCreation, $afterCreation);
+            $this->assertStringEndsWith('/created.php', $afterCreation);
+        } finally {
+            if (file_exists($directory . '/created.php')) {
+                unlink($directory . '/created.php');
+            }
+
+            rmdir($directory);
+        }
+    }
+}
