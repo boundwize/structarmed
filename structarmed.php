169ef6bfb2bbf69157c62277c83e37aaad4b18b5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-09-02 23:35:00 +0700

    cs fix

--- structarmed.php
+++ structarmed.php
@@ -46,10 +46,10 @@ return Architecture::define()
     ])
     ->skip([
         'tests/Fixtures/',
-        Psr1Preset::METHODS_MUST_BE_CAMEL_CASE                   => [
+        Psr1Preset::METHODS_MUST_BE_CAMEL_CASE                       => [
             __DIR__ . '/src/Preset/Preset.php',
         ],
-        Psr1Preset::FILES_SHOULD_DECLARE_SYMBOLS_OR_SIDE_EFFECTS => [
+        Psr1Preset::FILES_SHOULD_DECLARE_SYMBOLS_OR_SIDE_EFFECTS     => [
             __DIR__ . '/tests/Analyser/Parallel/ParallelAnalysisNodeExtractorTest.php',
             __DIR__ . '/tests/Analyser/Parallel/MockFunctions.php',
         ],
